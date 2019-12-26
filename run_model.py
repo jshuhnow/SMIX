5dea1b4a5b99b58655839105a629dea05b325f79
Jason Huh <jaeseok.huh.0@gmail.com>
2019-12-26 14:12:10 +0900

    Correct the progress output

--- run_model.py
+++ run_model.py
@@ -29,8 +29,8 @@ def test_model(model, env, num_runs=50):
             reward, terminated, info = env.step(actions[0])
             model.post_action(reward,terminated,info, actions)
 
-        if i%100 == 0:
-            print(f"{i} / {num_runs}")
+        if (i+1)%100 == 0:
+            print(f"{i+1} / {num_runs}")
         ##########################################################################
 
         if terminated:
